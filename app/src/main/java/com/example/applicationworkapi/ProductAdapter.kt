69b1eba871a567fbc41f939825b3e11c42cbc16b
Josebias Neto <josebiasbsneto@gmail.com>
2021-07-25 14:44:54 -0300

    Create adapter

---
+++ app/src/main/java/com/example/applicationworkapi/ProductAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.applicationworkapi
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.applicationworkapi.model.Product
+
+class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+}
+
+class ProductAdapter(
+    private val context:Context,
+    private val productList: List<Product>) : RecyclerView.Adapter<ProductViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        LayoutInflater
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
